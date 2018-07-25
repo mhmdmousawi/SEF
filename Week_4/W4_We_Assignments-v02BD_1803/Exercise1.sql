110c31530105ec8e603e799ef8392ac99554d140
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-07-25 11:19:41 +0300

    Add Query 3 + 4

--- Week_4/W4_We_Assignments-v02BD_1803/Exercise1.sql
+++ Week_4/W4_We_Assignments-v02BD_1803/Exercise1.sql
@@ -13,4 +13,24 @@ left join language as L on L.language_id = F.language_id
 where release_year = 2006
 group by L.name
 order by filmCount DESC
-limit 1
+limit 3
+
+--Query 3 (What are the top 3 countries from which customers are originating?)
+select C.country,count(Cus.customer_id)
+from customer as Cus
+left join address as A on A.address_id = Cus.address_id
+left join city as City on City.city_id = A.city_id
+left join country as C on C.country_id = City.country_id
+group by C.country
+order by count(Cus.customer_id) DESC
+limit 3
+
+--Query 4 (Find all the addresses where the second address is not empty 
+--(i.e., contains some text), and return these second addresses sorted.)
+select SecondAddresses.address from(
+select A.address2 as address, count(address_id) as num
+from address as A
+where A.address2 !=  ""
+group by A.address2
+order by num DESC
+) as SecondAddresses
