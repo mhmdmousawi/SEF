7ef3d7718a89e7f88393f7a7ca778066edeb0643
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-07-31 17:30:18 +0300

    Add Config.php in which it contain Database credentials

---
+++ Week_5/W5_Mo_Assignments-v02BD_1702/Config.php
@@ -0,0 +1,56 @@
+
+<?php
+
+class Config {
+
+	private $db_host ;
+	private $db_user ;
+	private $db_pass ;
+	private $db_name ;
+
+
+	function __construct()
+	{
+		echo "Config created";
+		$this->setDBHost('127.0.0.1'); 
+		$this->setDBUser('phpuser');
+		$this->setDBPassword('la2ya7obbi');
+		$this->setDBName('sakila');
+	}
+
+	function setDBHost($host)
+	{
+		$this->db_host = $host;
+	}
+	function setDBUser($user)
+	{
+		$this->db_user = $user;
+	}
+	function setDBPassword($pass)
+	{
+		$this->db_pass = $pass;
+	}
+	function setDBName($name)
+	{
+		$this->db_name = $name;
+	}
+
+	function getDBHost(){
+		return $this->db_host;
+	}
+	function getDBUser()
+	{
+		return $this->db_user;
+	}
+	function getDBPasword()
+	{
+		return $this->db_pass;
+	}
+	function getDBName()
+	{
+		return $this->db_name;
+	}
+}
+
+
+?>
