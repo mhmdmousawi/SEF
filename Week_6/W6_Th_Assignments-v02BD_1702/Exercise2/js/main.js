47d2b1a5bb1b79725cef549678a955751094dbaf
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-08-13 11:59:31 +0300

    Add script in which the user can delete a note

--- Week_6/W6_Th_Assignments-v02BD_1702/Exercise2/js/main.js
+++ Week_6/W6_Th_Assignments-v02BD_1702/Exercise2/js/main.js
@@ -1,10 +1,15 @@
 
 var APP =  {
-    notes_ids : [],
+    items_id : 0,
     btn_add : document.getElementById("btn_add"),
     input_title : document.getElementById("input_title"),
     input_desc : document.getElementById("input_desc"),
     items : document.getElementById("items"),
+    months : ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    days : ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    deleteItem : function(btn){
+        btn.parentNode.parentNode.remove();
+    },
     addItem : function(){
         let title = this.input_title.value;
         let desc = this.input_desc.value;
@@ -12,6 +17,7 @@ var APP =  {
         if(title != "" && desc != ""){
 
             let article__item_div = document.createElement("div");
+            article__item_div.id = "item__"+(++this.items_id);
             article__item_div.classList.add("article__item"); 
 
             let item__content_div = document.createElement("div");
@@ -23,7 +29,15 @@ var APP =  {
 
             let p_time = document.createElement("p");
             p_time.classList.add("content__time");
-            let time_p = "now";
+
+            let date = new Date();
+            let time_p = "added: "+
+                        this.days[date.getDay()]+ ", " +
+                        this.months[date.getMonth()]+" "+
+                        date.getDate()+" "+
+                        date.getFullYear() + " "+ 
+                        date.getHours() + ":" +
+                        date.getMinutes();
             p_time.innerHTML = time_p;
 
             let p_desc = document.createElement("p");
@@ -35,7 +49,9 @@ var APP =  {
             item__delete_div.classList.add("item__delete");
 
             let btn_delete = document.createElement("button");
+            btn_delete.id = "btn_"+ this.items_id;
             btn_delete.innerHTML = "X";
+            btn_delete.addEventListener('click', function(){this.deleteItem(btn_delete);}.bind(this));
 
             item__content_div.appendChild(h2);
             item__content_div.appendChild(p_time);
@@ -50,16 +66,19 @@ var APP =  {
         }else {
             alert("Please enter whatever..");
         }
-
+    },
+    expandArea: function(){
+        var heightLimit = 200;
+        this.input_desc.style.height = ""; 
+        this.input_desc.style.height = Math.min(this.input_desc.scrollHeight, heightLimit) + "px";
     },
     init : function()
     { 
-        this.btn_add.addEventListener('click',function(){this.addItem();}.bind(this))
+        this.btn_add.addEventListener('click',function(){this.addItem();}.bind(this));
+        this.input_desc.addEventListener('input', function(){this.expandArea();}.bind(this));
     }
     
 };
 var app = Object.create(APP);
 app.init();
 
-
-//$("#txtInput").autogrow();
