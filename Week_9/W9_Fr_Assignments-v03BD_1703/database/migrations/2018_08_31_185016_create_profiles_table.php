8b9625ba16d3c38724ab9e9b6d72641bb81b0c29
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-08-31 22:55:35 +0300

    Add Models: User, Profile, Channel, Participant, Chat. And migrations for related tables

---
+++ Week_9/W9_Fr_Assignments-v03BD_1703/database/migrations/2018_08_31_185016_create_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->Integer('user_id')->unsigned()->primary();
+            $table->string('full_name')->nullable();
+            $table->string('display_name')->nullable();
+            $table->mediumText('status')->nullablr();
+            $table->integer('phone');
+            $table->integer('profile_picture_id')->unsigned()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
--- Week_9/W9_Fr_Assignments-v03BD_1703/database/migrations/2018_08_31_193911_create_participants_table.php
+++ Week_9/W9_Fr_Assignments-v03BD_1703/database/migrations/2018_08_31_193911_create_participants_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateParticipantsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('participants', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('channel_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->boolean('removed')->default(false);
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('participants');
     }
 }
