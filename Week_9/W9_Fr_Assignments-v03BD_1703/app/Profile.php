57672dd95b90d1066e7fa6c61c88fa3d9802baab
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-09-01 02:57:40 +0300

    Update Profile model --add relation functions

--- Week_9/W9_Fr_Assignments-v03BD_1703/app/Profile.php
+++ Week_9/W9_Fr_Assignments-v03BD_1703/app/Profile.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo('App\User','id');
+    }
+
+    public function channel()
+    {
+        return $this->hasMany('App\Channel','creator_id');
+    }
+
+    public function participant()
+    {
+        return $this->hasMany('App\Participant','profile_id');
+    }
+
+    public function picture()
+    {
+        return $this->hasOne('App\Picture','id');
+    }
 }
