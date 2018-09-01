daf25423b6d60db70869a4249fa8f2a927be1ae0
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-09-01 03:06:19 +0300

    Update Participant model --add relation functions

--- Week_9/W9_Fr_Assignments-v03BD_1703/app/Participant.php
+++ Week_9/W9_Fr_Assignments-v03BD_1703/app/Participant.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Participant extends Model
 {
-    //
+    public function chat()
+    {
+        return $this->hasMany('App\Chat','id');
+    }
+
+    public function channel()
+    {
+        return $this->belongsTo('App\Channel','id');
+    }
+
+    public function profile()
+    {
+        return $this->belongsTo('App\Profile','profile_id');
+    }
 }
