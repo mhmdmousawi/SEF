759399a99fc0abbaf0ee97270803e8c32e309131
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-09-01 05:37:31 +0300

    Update Users Seeders

--- Week_9/W9_Fr_Assignments-v03BD_1703/database/seeds/UsersTableSeeder.php
+++ Week_9/W9_Fr_Assignments-v03BD_1703/database/seeds/UsersTableSeeder.php
@@ -13,7 +13,6 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
-        $names = ['samir','mhmd','ali','harout','yasmine','zeinab','samira'];
 
         for( $i = 1 ; $i< 50; $i++){
 
@@ -21,33 +20,6 @@ class UsersTableSeeder extends Seeder
                 'email' => $faker->unique()->safeEmail,
                 'password' => bcrypt('password')
             ]);
-            
         }
-
-        $channels_ids = App\Channel::all()->pluck('id')->toArray();
-
-        for( $i = 1 ; $i< 50; $i++){
-            
-            App\Participant::create([
-                'channel_id' => $faker->randomElement($channels_ids),
-                'profile_id' => $faker->randomElement($profiles_ids)
-            ]);
-        }
-        
-
-        
-
-        $participants_ids = App\Participant::all()->pluck('id')->toArray();
-
-        for( $i = 1 ; $i< 50; $i++){
-            
-            App\Chat::create([
-                'participant_id' => $faker->randomElement($participants_ids),
-                'content' => $faker->sentence
-            ]);
-        }
-
-        
-
     }
 }
