aec5b63a512ca89bff69b491163698e6a0594778
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-09-01 03:01:47 +0300

    Update Channel model --add relation functions

--- Week_9/W9_Fr_Assignments-v03BD_1703/app/Channel.php
+++ Week_9/W9_Fr_Assignments-v03BD_1703/app/Channel.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Channel extends Model
 {
-    //
+    public function profile()
+    {
+        return $this->belongsTo('App\Profile','profile_id');
+    }
+
+    public function participant()
+    {
+        return $this->hasMany('App\Participant','id');
+    }
+
+    public function picture()
+    {
+        return $this->hasOne('App\Picture','id');
+    }
 }
