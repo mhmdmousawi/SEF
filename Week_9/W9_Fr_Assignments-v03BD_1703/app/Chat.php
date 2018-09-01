62559f362e21c841a4668ff94bc4aa8f51289602
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-09-01 03:03:27 +0300

    Update Chat model --add relation functions

--- Week_9/W9_Fr_Assignments-v03BD_1703/app/Chat.php
+++ Week_9/W9_Fr_Assignments-v03BD_1703/app/Chat.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    //
+    public function participant()
+    {
+        return $this->belongsTo('App\Participant','id');
+    }
 }
