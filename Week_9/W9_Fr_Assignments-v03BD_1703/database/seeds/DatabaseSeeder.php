be9df4c94c2d74a0eb724873add3fcc6cfbb358e
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-09-01 05:39:58 +0300

    Update Database Seeders

--- Week_9/W9_Fr_Assignments-v03BD_1703/database/seeds/DatabaseSeeder.php
+++ Week_9/W9_Fr_Assignments-v03BD_1703/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+        $this->call(ProfilesTableSeeder::class);
+        $this->call(ChannelsTableSeeder::class);
+        $this->call(ParticipantsTableSeeder::class);
+        $this->call(ChatsTableSeeder::class);
     }
 }
