42d7539630b9f3742b1b1e499bd31a87ddf09ee1
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-09-01 03:07:43 +0300

    Update Picture model --add relation functions

--- Week_9/W9_Fr_Assignments-v03BD_1703/app/Picture.php
+++ Week_9/W9_Fr_Assignments-v03BD_1703/app/Picture.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Picture extends Model
 {
-    //
+    public function profile()
+    {
+        return $this->belongsTo('App\Profile','profile_id');
+    }
+
+    public function channel()
+    {
+        return $this->belongsTo('App\Channel','id');
+    }
 }
