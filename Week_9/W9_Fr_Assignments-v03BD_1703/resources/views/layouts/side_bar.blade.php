3be47fa7596019395b4b122cdbfa60c95cabc30a
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-09-01 16:02:24 +0300

    Update side_bar blade

--- Week_9/W9_Fr_Assignments-v03BD_1703/resources/views/layouts/side_bar.blade.php
+++ Week_9/W9_Fr_Assignments-v03BD_1703/resources/views/layouts/side_bar.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-xl-12">
             <div class="card">
                 <div class="card-header">Side Bar</div>
 
@@ -11,20 +11,27 @@
                     <div class="side_bar">
                         <header class="side_bar__header">
                             <h2>{{ config('app.name', 'Laravel') }}</h2>
-                            <p>Status<p>
-                            <p>{{$profile->display_name}}<p>
+                            <div class="status status-active"></div>
+                            <div class="header__name">
+                                <p>{{$profile->display_name}}<p>
+                            </div>
                         </header>
                         <hr>
                         <div class="side_bar__channels">
                             <header class="channel__title">
+                                <a href="#" class="btn-add"><p>+</p></a>
                                 <h4>Channels</h4>
-                                <a class="btn btn-primary btn-sm btn-add">+</a>
                             </header>
                             <article class="channel__names">
                                 @foreach($profile->channels as $channel)
-                                <div class="channel_name">
-                                    <p>{{$channel->name}}</p>
-                                </div>
+                                @if($channel->private)   
+                                    <div class="private-status private-status-on"></div>
+                                @else
+                                    <div class="private-status"></div>
+                                @endif
+                                    <div class="channel_name">
+                                        <a href="#"><p>{{$channel->name}}</p></a>
+                                    </div>
                                 @endforeach
                             </article>
                             
@@ -32,13 +39,14 @@
                         <hr>    
                         <div class="side_bar__dm">
                             <header class="channel__title">
+                                <a href="#" class="btn-add"><p>+</p></a>
                                 <h4>Direct Messages</h4>
-                                <a class="btn btn-primary btn-sm btn-add">+</a>
                             </header>
                             <article class="dm__names">
                                 @foreach($profile->friends as $friend)
+                                    <div class="status"></div>
                                     <div class="dm_name">
-                                        <p>{{$friend->display_name}}</p>
+                                        <a href="#"><p>{{$friend->display_name}}</p></a>
                                     </div>
                                 @endforeach
                             </article>
