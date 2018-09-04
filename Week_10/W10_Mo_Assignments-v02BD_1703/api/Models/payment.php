e0b9c7d78d500dfcd6e2b34b59743471797031aa
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-09-04 11:03:43 +0300

    Add model payment that containes all table attributes

---
+++ Week_10/W10_Mo_Assignments-v02BD_1703/api/Models/payment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Models\Payment;
+
+class Payment{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "payment";
+ 
+    // Actor properties
+    public $payment_id;
+    public $customer_id;
+    public $staff_id;
+    public $rental_id;
+    public $amount;
+    public $payment_date;
+    public $last_update;
+ 
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+}
