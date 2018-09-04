5543c674072f21f7d306689342073c04ae7851a4
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-09-04 11:05:51 +0300

    Add model customer that containes all table attributes

---
+++ Week_10/W10_Mo_Assignments-v02BD_1703/api/Models/customer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Models\Customer;
+
+class Customer{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "customer";
+ 
+    // Actor properties
+    public $customer_id;
+    public $store_id;
+    public $first_name;
+    public $last_name;
+    public $email;
+    public $address_id;
+    public $active;
+    public $create_date;
+    public $last_update;
+ 
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+}
