c2edecefbebbe944c451bafa03de72b4c377234a
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-09-04 11:09:58 +0300

    Add model film that containes all table attributes

---
+++ Week_10/W10_Mo_Assignments-v02BD_1703/api/Models/film.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Models\Film;
+
+class Film{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "film";
+ 
+    // Actor properties
+    public $film_id;
+    public $title;
+    public $description;
+    public $release_year;
+    public $language_id;
+    public $original_language_id;
+    public $rental_duration;
+    public $rental_rate;
+    public $length;
+    public $replacement_cost;
+    public $rating;
+    public $special_features;
+    public $last_update;
+ 
+    // constructor with $db as database connection
+    public function __construct($db){
+        $this->conn = $db;
+    }
+}
