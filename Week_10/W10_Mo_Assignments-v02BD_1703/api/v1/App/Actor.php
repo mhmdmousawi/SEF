f363292f133d85e1593319c4cf2e3f8f2e27ea86
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-09-07 10:17:23 +0300

    Update api folder structure

---
+++ Week_10/W10_Mo_Assignments-v02BD_1703/api/v1/App/Actor.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__.'/../Model.php';
+
+class Actor extends Model
+{
+    public $first_name;
+    public $last_name;
+    public $attributes;
+
+    public $fillables = 'first_name, last_name';
+    public $values = [];
+
+    //values and fillables should match
+    // public function setValues()
+    // {
+    //     $this->values = [
+    //         $this->first_name,
+    //         $this->last_name
+    //     ];
+    // } 
+}
