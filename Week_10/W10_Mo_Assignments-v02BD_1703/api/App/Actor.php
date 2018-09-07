36c8bc4952a802f23c5d49d3a0736c19b21a6ac3
mhmdmousawi <mhmd.mousawi@hotmail.com>
2018-09-07 10:19:33 +0300

    Update api folder structure

--- Week_10/W10_Mo_Assignments-v02BD_1703/api/App/Actor.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__.'/../Model.php';
-
-class Actor extends Model
-{
-    
-}
--- Week_10/W10_Mo_Assignments-v02BD_1703/api/v1/Model.php
+++ Week_10/W10_Mo_Assignments-v02BD_1703/api/v1/Model.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once 'config/database.php';
+require_once 'config/database.php';
 
 abstract class Model {
 
@@ -8,14 +8,15 @@ abstract class Model {
     private $conn;
     protected $table_name;
     private $query ;
+    public $fillables = " ";
+    public $values = [];
 
     //filters
     private $select_filter = " * ";
     private $limit_filter = " ";
     private $order_by_filter = " ";
     private $where_filter = " ";
-
-    // abstract function setTableName($table_name);
+    private $set_filter = " ";
 
     public function __construct()
     {
@@ -60,7 +61,7 @@ abstract class Model {
         return $this;
     }
 
-    public function where($attribute,$operator = '=', $value)
+    public function where($attribute,$operator = " = ", $value)
     {
         if($this->where_filter == " "){
             $this->where_filter = " WHERE " . $attribute . " " . $operator . " '" . $value ."' ";
@@ -80,19 +81,110 @@ abstract class Model {
         return $this;
     }
 
+    public function set($attribute,$value)
+    {
+        
+        if($this->set_filter == " "){
+            $this->set_filter = " SET " . $attribute . " = '" . $value ."' ";
+        }else{
+            $this->set_filter .= " , " . $attribute . " = '" . $value ."' ";
+        }
+        return $this;
+    }
+
     public function get()
     {
-        $this->setQuery();
+        $this->setQuerySelect();
         return $this->result();
     }
 
-    private function setQuery()
+    public function setValues(){}
+
+    public function save()
+    {
+        if($this->fillables==" "){
+            return $this->errorMsg();
+        }
+
+        $attributes = $this->fillables;
+        $this->setValues();
+        //add "'" to each value
+        $values_string = "";
+        $values_num = count($this->values);
+        $i = 0;
+
+        foreach ($this->values as $value) {
+            $i++;
+            if( $i ==  $values_num ){
+                $values_string .= "'".$value."'";
+            }else{
+                $values_string .= "'".$value."',";
+            }
+        } //values_string as string ex. "'mhmd','mousawi'"
+
+        $this->query = " INSERT INTO " . $this->table_name ." ".
+                        " ( " .$attributes . " ) ".
+                        " VALUES ".
+                        " ( " .$values_string . " ) ";
+
+        $stmt = $this->conn->prepare($this->query);
+
+        // execute query
+        if(!$stmt->execute()){
+            return $this->errorMsg();
+        }
+
+        // $last_id = $this->conn->lastInsertId();
+
+        return $this->successMsg();     
+    }
+
+    public function update()
+    {
+        $this->setQueryUpdate();
+        return $this->msgResult();
+    }
+
+    public function delete()
+    {
+        $this->setQueryDelete();
+        return $this->msgResult();
+    }
+
+    private function setQueryDelete()
+    {
+        $this->query = " DELETE FROM " . $this->table_name . " ".
+                        $this->where_filter . " ";
+    }
+
+    private function setQueryUpdate()
+    {
+        $this->query = " UPDATE " . $this->table_name . " ".
+                        $this->set_filter . " " .
+                        $this->where_filter . " ";
+    }
+
+
+    private function setQuerySelect()
     {
         $this->query = " SELECT ".$this->select_filter ." ".
                         " FROM " . $this->table_name . " ".
                         $this->where_filter . " ". 
                         $this->order_by_filter . " ". 
                         $this->limit_filter. " ";
+    }
+
+    private function msgResult()
+    {
+        // prepare query statement
+        $stmt = $this->conn->prepare($this->query);
+        // execute query
+        if($stmt->execute()){
+
+            return $this->successMsg();
+        }else{
+            return $this->errorMsg();
+        }
     }
 
     private function result()
@@ -111,9 +203,7 @@ abstract class Model {
          
             // retrieve our table contents
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-
                 array_push($result_array["records"], $row);
-                
             }
          
             $result_array["message"]= "success";
@@ -121,9 +211,18 @@ abstract class Model {
         }
          
         else{
-            return array("message" => "fail");
+            return $this->errorMsg();
         }
     }
 
-    
+    public function successMsg()
+    {
+        return array("message" => "success");
+    }
+
+    public function errorMsg()
+    {
+        return array("message" => "fail");
+    }
+
 }
